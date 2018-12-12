2e7276839c21c435154d315f8944455248897262
Hatice Melike Ecevit <32355785+haticemelikeecevit@users.noreply.github.com>
2018-12-12 13:48:31 +0300

    Create rangeSumOfBST.java

---
+++ rangeSumOfBST.java
@@ -0,0 +1,26 @@
+/*
+Given the root node of a binary search tree, return the sum of values of all nodes with value between L and R (inclusive).
+
+The binary search tree is guaranteed to have unique values.
+*/
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public int rangeSumBST(TreeNode root, int L, int R) {
+        if(root == null)
+            return 0;
+        int sum = 0;
+        if (root.val >= L && root.val <= R)
+            sum += root.val;
+        sum =  sum + rangeSumBST(root.left, L, R);
+        sum =  sum + rangeSumBST(root.right, L, R);
+        return sum;
+    }
+}
